3fdf253d0de06f56215a4da35ec60d3176716d02
Arman <tajmohol559gmail.com>
2024-08-24 13:57:08 +0600

    rating icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,62 @@
-
-import { useEffect } from 'react';
-import { useState } from 'react';
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 import axios from 'axios';
 import { Col, Container, Row } from 'react-bootstrap';
-import { FaRegStar } from "react-icons/fa";
+import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
 
+function Rating({ rating }) {
+  const fullStars = Math.floor(rating);
+  const halfStar = rating - fullStars >= 0.5;
+  const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
+
+  return (
+    <div className="rating-container">
+      <span className="rating-text">Rating: {rating.toFixed(1)}</span>
+      <span className="icon">
+        {[...Array(fullStars)].map((_, i) => <FaStar key={`full-${i}`} color="#FFD700" />)}
+        {halfStar && <FaStarHalfAlt color="#FFD700" />}
+        {[...Array(emptyStars)].map((_, i) => <FaRegStar key={`empty-${i}`} color="#FFD700" />)}
+      </span>
+    </div>
+  );
+}
 
 function App() {
   let [product, setProduct] = useState([]);
-  let getdata = () =>{
-    axios.get('https://dummyjson.com/products').then((respone)=>{
-      setProduct(respone.data.products);
-      
+  
+  let getdata = () => {
+    axios.get('https://dummyjson.com/products').then((response) => {
+      setProduct(response.data.products);
     });
   }
-  useEffect(() =>{
+
+  useEffect(() => {
     getdata();
-  },[])
+  }, []);
+
   return (
-    
     <>
-    <Container>
-      <Row>
-        {product.map((item, i)=>(
-        <Col key={i} lg={4}>
-
-        <div className="Metarial">
-          <img src={item.thumbnail} alt="" />
-         <h3> {item.title}</h3>
-         <p> {`${item.description.slice(0,50)}.. see more`}</p>
-         <h3> {item.stock}</h3>
-         <h3>Rating: {item.rating}<FaRegStar /><FaRegStar />
-         <FaRegStar />
-         <FaRegStar />
-
-         </h3>
-         <h3>Price: ${item.price}</h3>
-         <h3>Discount: {item.discountPercentage}%</h3>
-        </div>
-        </Col>
-        ))}
-       
-      </Row>
-    </Container>
-
+      <Container>
+        <Row>
+          {product.map((item, i) => (
+            <Col key={i} lg={4}>
+              <div className="Metarial">
+                <img src={item.thumbnail} alt={item.title} />
+                <h3>{item.title}</h3>
+                <p>{`${item.description.slice(0, 50)}... see more`}</p>
+                <h3>Stock: {item.stock}</h3>
+                <div className="rating-wrapper">
+                  <Rating rating={item.rating} />
+                </div>
+                <h3>Price: ${item.price}</h3>
+                <h3>Discount: {item.discountPercentage}%</h3>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
